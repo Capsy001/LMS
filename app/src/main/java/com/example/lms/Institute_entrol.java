2a034d03a882e5039f856ae4084b0f618e2735c7
chamuditha4 <36603594+chamuditha4@users.noreply.github.com>
2021-08-13 19:23:17 +0530

    Admin + institute activity

---
+++ app/src/main/java/com/example/lms/Institute_entrol.java
@@ -0,0 +1,14 @@
+package com.example.lms;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Institute_entrol extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.institutes_entroll);
+    }
+}
--- app/src/main/java/com/example/lms/Institutes.java
+++ app/src/main/java/com/example/lms/Institutes.java
@@ -1,19 +1,35 @@
 package com.example.lms;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
-public class Institutes extends Fragment {
+public class Institutes extends Fragment implements View.OnClickListener {
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.institutes,container,false);
+        View v = inflater.inflate(R.layout.institutes,container,false);
+
+        Button b = (Button) v.findViewById(R.id.button3);
+        b.setOnClickListener(this);
+        return v;
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.button3:
+                Intent intent = new Intent(getActivity(), Institute_entrol.class);
+                startActivity(intent);
+                break;
+        }
     }
 }
