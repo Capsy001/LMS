623d42a8ad4de4430d1b0468383bcacc0722e828
chamuditha4 <36603594+chamuditha4@users.noreply.github.com>
2021-08-14 10:13:36 +0530

    My Course

--- app/src/main/java/com/example/lms/MyCourse.java
+++ app/src/main/java/com/example/lms/MyCourse.java
@@ -4,6 +4,8 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -16,6 +18,12 @@ public class MyCourse extends Fragment {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.course,container,false);
+        View v = inflater.inflate(R.layout.course,container,false);
+        Spinner spinner = (Spinner) v.findViewById(R.id.mycourse);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this.getActivity(), R.array.assignments, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_dropdown_item_1line);
+        spinner.setAdapter(adapter);
+
+        return v;
     }
 }
